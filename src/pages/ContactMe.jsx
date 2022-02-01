9bdb3c85bbe1b249a8e2f5e09cd3b8a2727b1467
ankittt20 <92395053+ankittt20@users.noreply.github.com>
2022-02-02 00:10:24 +0530

    Import createDomMotionComponent.

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from "react";
 import Form from "../components/Form/Form";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { animationOne } from "../components/Animation";
 import Loader from "../components/Loader";
+
+import { createDomMotionComponent } from "framer-motion";
+
+/** IE 11 supported version of the motion component */
+const motion = {
+  div: createDomMotionComponent("div"),
+  span: createDomMotionComponent("span"),
+  svg: createDomMotionComponent("svg")
+};
 
 function ContactMe() {
   const [loading, setLoading] = useState(false);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,17 @@ import Sidebar from "../components/Sidebar/Sidebar";
 import About from "../components/About/about";
 import Contact from "../components/Contact/contact";
 import Skills from "../components/Skills/Skills";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { animationOne, transition } from "../components/Animation";
+
+import { createDomMotionComponent } from "framer-motion";
+
+/** IE 11 supported version of the motion component */
+const motion = {
+  div: createDomMotionComponent("div"),
+  span: createDomMotionComponent("span"),
+  svg: createDomMotionComponent("svg")
+};
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
