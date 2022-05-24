5b7991d71ed27a8fd3d4184c9965e4fcc2ac2f43
Ankit Kumar <ankitkumar3624@gmail.com>
2022-05-24 16:09:00 +0530

    Contact Page Change

--- src/components/Contact/ContactBG.jsx
+++ src/components/Contact/ContactBG.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 export const ContactBG = styled.div`
   border-radius: 25px;
   /* background: #f1f1e8; */
-  background: rgb(176, 187, 255);
+  ${'' /* background: rgb(176, 187, 255);
   background: linear-gradient(
     rgba(176, 187, 255, 1) 0%,
     rgba(0, 41, 113, 1) 100%
-  );
+  ); */}
+  background: #191919;
   padding: 20px;
   box-shadow: 0 19px 51px 0 rgba(0, 0, 0, 0.16),
     0 14px 19px 0 rgba(0, 0, 0, 0.07);
@@ -27,13 +28,24 @@ export const ContactBG = styled.div`
 `;
 
 export const Heading = styled.h1`
+  background: linear-gradient(
+    271deg,
+    #a0e9ff 30%,
+    #a162e8 50%,
+    #f093b0 70%,
+    #edca85 94%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 3.5rem;
+  line-height: 120%;
+  letter-spacing: -1px;
   text-align: center;
-  margin: 12% auto 3%;
-  font-size: 4rem;
-  font-family: "Prata";
+  margin: 12% 20% 3%;
+  font-family: "MyWebFont", cursive;
   @media screen and (max-width: 992px) {
     font-size: 4rem;
-    margin: 10% auto 2%;
+    margin: 15% auto 2%;
   }
 `;
 
